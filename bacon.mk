d12168b9335b304785ad8064c07473b3caaab6c5
Luca Stefani <luca020400@lineageos.org>
2017-09-26 05:22:08 +0000

    msm8974: Remove no longer used hostapd configs
    Change-Id: Ibb4db7d65b9e7d34ea3ca2f21529e404a21b3ac2

--- bacon.mk
+++ bacon.mk
@@ -348,9 +348,6 @@ PRODUCT_PACKAGES += \
     wifilogd
 
 PRODUCT_PACKAGES += \
-    hostapd_default.conf \
-    hostapd.accept \
-    hostapd.deny \
     wpa_supplicant.conf \
     wpa_supplicant_overlay.conf \
     p2p_supplicant_overlay.conf
