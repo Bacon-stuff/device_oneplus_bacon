80f51e47dbda71c13e0de7a6d219c92d7dbb24e2
kishpatel1998 <kishanppatel19@gmail.com>
2018-01-06 07:50:04 +0000

    Revert "bacon: Import some kernel-headers"
    This reverts commit 8b32438218a7404ecc0e697ce26d22687647238f.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -55,7 +55,6 @@ TARGET_KERNEL_ARCH := arm
 TARGET_KERNEL_CONFIG := bacon_defconfig
 TARGET_KERNEL_SOURCE := kernel/oneplus/msm8974
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
-PRODUCT_VENDOR_KERNEL_HEADERS :=  $(DEVICE_PATH)/kernel-headers
 
 # ANT+
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
