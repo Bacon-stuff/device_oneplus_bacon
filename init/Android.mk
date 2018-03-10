c3a2e4a4924777293837c20ea1050f86e2889861
kishpatel1998 <kishanppatel19@gmail.com>
2018-03-10 11:43:24 +0000

    Revert "bacon: init: Update for O"
    This reverts commit 57681d2a663905326adc8b5c5f9f47942e1de3a9.

--- init/Android.mk
+++ init/Android.mk
@@ -3,9 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
-LOCAL_C_INCLUDES := \
-    system/core/init \
-    external/selinux/libselinux/include
+LOCAL_C_INCLUDES := system/core/init
 LOCAL_CFLAGS := -Wall
 LOCAL_STATIC_LIBRARIES := libbase
 LOCAL_SRC_FILES := init_bacon.cpp
