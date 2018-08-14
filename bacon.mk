ae7750258fd0430720f13f85dd608b35856b7bad
Kshitij Gupta <kshitijgm@gmail.com>
2018-08-14 16:16:50 +0000

    bacon: Import modified android.hardware.bluetooth@1.0-impl
    - Import and include modified HIDL
    - Is same as AOSP but has
      https://android.googlesource.com/platform/hardware/interfaces/+/8d0d6f99dfbd121140549adc43f360686467e875
      reverted
    
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- bacon.mk
+++ bacon.mk
@@ -65,7 +65,7 @@ PRODUCT_PACKAGES += \
 
 # Bluetooth HAL
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl \
+    android.hardware.bluetooth@1.0-impl.bacon \
     libbt-vendor
 
 PRODUCT_COPY_FILES += \
